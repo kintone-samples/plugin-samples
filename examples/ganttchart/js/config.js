8c7a2a91eca2d25262dbd7806c0eecdd9a4f90d5
Ushirosako <t-ushirosako@cybozu.co.jp>
2014-12-25 17:08:36 +0900

    add ganttchart example

---
+++ examples/ganttchart/js/config.js
@@ -0,0 +1,176 @@
+jQuery.noConflict();
+
+(function($, PLUGIN_ID){
+    "use strict";
+
+    $(document).ready(function(){
+    
+        var terms = {
+            'en':{
+                'ganttchartTitle': 'Title',
+                'ganttchartTitle_label': 'Field of title.',
+                'ganttchartTitle_description': 'Please specify the fields to be displayed in the first classification of the vertical axis of the Gantt chart.',
+                'ganttchartDesc': 'Subtitle',
+                'ganttchartDesc_label': 'Field of subtitle',
+                'ganttchartDesc_description': 'Please specify the fields to be displayed in the second classification of the vertical axis of the Gantt chart.',
+                'ganttchartFrom': 'Start date',
+                'ganttchartFrom_label': 'Field of start date',
+                'ganttchartFrom_description': '',
+                'ganttchartTo': 'End date',
+                'ganttchartTo_label': 'Field of End date',
+                'ganttchartTo_description': '',
+                'ganttchartColor': 'Color of chart',
+                'ganttchartColor_label': 'Field of color',
+                'ganttchartColor_label_name': 'Color',
+                'ganttchartColor_label_setting': 'Settings',
+                'ganttchartColor_description': 'Please enter the condition value coloring to. (More certain when example of "A, B, C") if the condition value is duplicated, it will be priority from the top.',
+                'ganttchartColor_red': 'Red',
+                'ganttchartColor_red_name': 'Red',
+                'ganttchartColor_orange': 'Orange',
+                'ganttchartColor_orange_name': 'Orange',
+                'ganttchartColor_green': 'Green',
+                'ganttchartColor_green_name': 'Green',
+                'ganttchartColor_blue': 'Blue',
+                'ganttchartColor_blue_name': 'Blue',
+                'ganttchartColor_gray': 'Gray',
+                'ganttchartColor_gray_name': 'Gray',
+                'ganttchartScall': 'Scall',
+                'ganttchartScall_label': 'Scale to be displayed early (day, month, year).',
+                'ganttchartScall_description': '',
+                'error': 'Error: ',
+                'plugin_submit': '     Save   ',
+                'plugin_cancel': '     Cancel   ',
+                'required_field': 'Required field is empty.',
+            },
+            'ja':{
+                'ganttchartTitle': 'タイトル',
+                'ganttchartTitle_label': 'タイトルにするフィールド',
+                'ganttchartTitle_description': 'ガントチャートの縦軸の第一分類に表示するフィールドを指定してください。',
+                'ganttchartDesc': 'サブタイトル',
+                'ganttchartDesc_label': 'サブタイトルにするフィールド',
+                'ganttchartDesc_description': 'ガントチャートの縦軸の第二分類に表示するフィールドを指定してください。',
+                'ganttchartFrom': '開始日',
+                'ganttchartFrom_label': '開始日のフィールド',
+                'ganttchartFrom_description': '',
+                'ganttchartTo': '終了日',
+                'ganttchartTo_label': '終了日のフィールド',
+                'ganttchartTo_description': '',
+                'ganttchartColor': 'チャートカラー',
+                'ganttchartColor_label': '色付けするフィールド',
+                'ganttchartColor_label_name': 'カラー',
+                'ganttchartColor_label_setting': '設定値',
+                'ganttchartColor_description': '色付けする条件値を入力してください。（複数ある際の例「A,B,C」） 条件値が重複する場合には、上から優先されます。',
+                'ganttchartColor_red': '赤',
+                'ganttchartColor_red_name': '赤',
+                'ganttchartColor_orange': 'オレンジ',
+                'ganttchartColor_orange_name': 'オレンジ',
+                'ganttchartColor_green': '緑',
+                'ganttchartColor_green_name': '緑',
+                'ganttchartColor_blue': '青',
+                'ganttchartColor_blue_name': '青',
+                'ganttchartColor_gray': 'グレー',
+                'ganttchartColor_gray_name': 'グレー',
+                'ganttchartScall': 'スケール',
+                'ganttchartScall_label': '初期に表示するスケール（日、月、年）',
+                'ganttchartScall_description': '',
+                'error': 'エラー: ',
+                'plugin_submit': '     保存   ',
+                'plugin_cancel': '  キャンセル   ',
+                'required_field': '必須項目が入力されていません。',
+            }
+        };
+        
+        // To switch the display by the login user's language (English display in the case of Chinese)
+        var lang = kintone.getLoginUser().language;
+        var i18n = (lang in terms) ? terms[lang]: terms['en'];
+        
+        var configHtml = $('#ganttchart-plugin').html();
+        var tmpl = $.templates(configHtml);
+        $('div#ganttchart-plugin').html(tmpl.render({'terms': i18n}));
+        
+        // Set in the item selection box retrieves the form information design
+        kintone.api(kintone.api.url('/k/v1/preview/form',true), 'GET', {'app': kintone.app.getId()}, function(resp) {
+            for (var i = 0; i < resp.properties.length; i++) {
+                var prop = resp.properties[i];
+                
+                switch (prop['type']){
+                case 'SINGLE_LINE_TEXT': 
+                case 'MULTI_LINE_TEXT': 
+                    $('#ganttchart-plugin-title').append($('<option>').text(prop['label']).val(prop['code']));
+                    $('#ganttchart-plugin-desc').append($('<option>').text(prop['label']).val(prop['code']));
+                    break;
+                case 'DATE': 
+                case 'DATETIME': 
+                    $('#ganttchart-plugin-from').append($('<option>').text(prop['label']).val(prop['code']));
+                    $('#ganttchart-plugin-to').append($('<option>').text(prop['label']).val(prop['code']));
+                    break;
+                case 'RADIO_BUTTON': 
+                case 'DROP_DOWN': 
+                    $('#ganttchart-plugin-color').append($('<option>').text(prop['label']).val(prop['code']));
+                    break;
+                }
+            }
+            
+            // Get the plug-in information to set the definition data
+            var config = kintone.plugin.app.getConfig(PLUGIN_ID);
+            config['ganttchartTitle'] && $('#ganttchart-plugin-title').val(config['ganttchartTitle']);
+            config['ganttchartDesc'] && $('#ganttchart-plugin-desc').val(config['ganttchartDesc']);
+            config['ganttchartFrom'] && $('#ganttchart-plugin-from').val(config['ganttchartFrom']);
+            config['ganttchartTo'] && $('#ganttchart-plugin-to').val(config['ganttchartTo']);
+            config['ganttchartColor'] && $('#ganttchart-plugin-color').val(config['ganttchartColor']);
+            config['ganttchartColor_red'] && $('#ganttchart-plugin-color-red').val(config['ganttchartColor_red']);
+            config['ganttchartColor_orange'] && $('#ganttchart-plugin-color-orange').val(config['ganttchartColor_orange']);
+            config['ganttchartColor_green'] && $('#ganttchart-plugin-color-green').val(config['ganttchartColor_green']);
+            config['ganttchartColor_blue'] && $('#ganttchart-plugin-color-blue').val(config['ganttchartColor_blue']);
+            config['ganttchartColor_gray'] && $('#ganttchart-plugin-color-gray').val(config['ganttchartColor_gray']);
+            config['ganttchartScall'] && $('#ganttchart-plugin-scall').val(config['ganttchartScall']);
+    
+        });
+    
+        // Save the value
+        $('#plugin_submit').click(function() {
+    
+            var ganttchartTitle = $('#ganttchart-plugin-title').val();
+            var ganttchartDesc = $('#ganttchart-plugin-desc').val();
+            var ganttchartFrom = $('#ganttchart-plugin-from').val();
+            var ganttchartTo = $('#ganttchart-plugin-to').val();
+            var ganttchartColor = $('#ganttchart-plugin-color').val();
+            var ganttchartColor_red = $('#ganttchart-plugin-color-red').val();
+            var ganttchartColor_orange = $('#ganttchart-plugin-color-orange').val();
+            var ganttchartColor_green = $('#ganttchart-plugin-color-green').val();
+            var ganttchartColor_blue = $('#ganttchart-plugin-color-blue').val();
+            var ganttchartColor_gray = $('#ganttchart-plugin-color-gray').val();
+            var ganttchartScall = $('#ganttchart-plugin-scall').val();
+    
+            // Check the required fields       
+            if (ganttchartTitle === '') {alert(i18n.required_field);return;}
+            if (ganttchartFrom === '') {alert(i18n.required_field);return;}
+            if (ganttchartTo === '') {alert(i18n.required_field);return;}
+            if (ganttchartColor === '') {alert(i18n.required_field);return;}
+            if (ganttchartScall === '') {alert(i18n.required_field);return;}
+            
+            // Set the definition data
+            var config = {};
+            config['ganttchartTitle'] = ganttchartTitle;
+            config['ganttchartDesc'] = ganttchartDesc;
+            config['ganttchartFrom'] = ganttchartFrom;
+            config['ganttchartTo'] = ganttchartTo;
+            config['ganttchartColor'] = ganttchartColor;
+            config['ganttchartColor_red'] = ganttchartColor_red;
+            config['ganttchartColor_orange'] = ganttchartColor_orange;
+            config['ganttchartColor_green'] = ganttchartColor_green;
+            config['ganttchartColor_blue'] = ganttchartColor_blue;
+            config['ganttchartColor_gray'] = ganttchartColor_gray;
+            config['ganttchartScall'] = ganttchartScall;
+            
+            kintone.plugin.app.setConfig(config);
+            
+        });
+    
+        // Clear the value
+        $('#plugin_cancel').click(function() {
+            history.back();
+        });
+    
+    });
+})(jQuery, kintone.$PLUGIN_ID);
---
+++ examples/ganttchart/js/desktop-ganttchart.js
@@ -0,0 +1,209 @@
+/*
+ * Gantt chart display of sample program
+ * Copyright (c) 2014 Cybozu
+ *
+ * Licensed under the MIT License
+ */
+jQuery.noConflict();
+
+(function($, PLUGIN_ID) {
+
+    "use strict";
+
+    // Record list of events.
+    kintone.events.on('app.record.index.show', function(event) {
+
+        var conf;
+        var GANTT_NAME, GANTT_DESC, GANTT_FROM, GANTT_TO, GANTT_SCALL;
+        var GANTT_TIP, GANTT_LABEL, GANTT_COLOR, GANTT_RED, GANTT_ORANGE, GANTT_GREEN, GANTT_BLUE, GANTT_GRAY;
+        var GANTT_MONTHS, GANTT_DOW, GANTT_WAIT = "";
+
+        // Use the plug-in.
+        if (PLUGIN_ID) {
+            conf = kintone.plugin.app.getConfig(PLUGIN_ID);
+            if (conf) {
+                GANTT_NAME = conf['ganttchartTitle'];
+                GANTT_DESC = conf['ganttchartDesc'];
+                GANTT_FROM = conf['ganttchartFrom'];
+                GANTT_TO = conf['ganttchartTo'];
+                GANTT_SCALL = conf['ganttchartScall'];
+                GANTT_COLOR = conf['ganttchartColor'];
+                GANTT_TIP = conf['ganttchartTitle'];
+                GANTT_LABEL = conf['ganttchartTitle'];
+                GANTT_NAME = conf['ganttchartTitle'];
+                GANTT_RED = conf['ganttchartColor_red'];
+                GANTT_ORANGE = conf['ganttchartColor_orange'];
+                GANTT_GREEN = conf['ganttchartColor_green'];
+                GANTT_BLUE = conf['ganttchartColor_blue'];
+                GANTT_GRAY = conf['ganttchartColor_gray'];
+            }
+        // Set when utilized in JavaScript read without using a plug-in.
+        }else{
+            GANTT_NAME = "To_Do";
+            GANTT_DESC = "Details";
+            GANTT_FROM = "From";
+            GANTT_TO = "To";
+            GANTT_TIP = "To_Do";
+            GANTT_LABEL = "To_Do";
+            GANTT_SCALL = "days"; // days,weeks,months
+            GANTT_COLOR = "Priority";
+            GANTT_RED = "A";
+            GANTT_ORANGE = "B";
+            GANTT_GREEN = "C";
+            GANTT_BLUE = "D";
+            GANTT_GRAY = "";
+        }
+
+        var records = event.records;
+        var data = [];
+
+        // Don't display when there is no record.
+        if (records.length === 0) {
+            return;
+        }
+        var elSpace = kintone.app.getHeaderSpaceElement();
+
+        // I will adjust the style depending on the version of the design
+        var uiVer = kintone.getUiVersion();
+        switch (uiVer) {
+            case 1:
+                elSpace.style.marginLeft = '28px';
+                elSpace.style.marginRight = '28px';
+                elSpace.style.border = 'solid 1px #ccc';
+                break;
+            default:
+                elSpace.style.marginLeft = '15px';
+                elSpace.style.marginRight = '15px';
+                elSpace.style.border = 'solid 1px #ccc';
+                break;
+        }
+
+        // I create an element of Gantt chart.
+        var elGantt = document.getElementById('gantt');
+        if (elGantt === null) {
+            elGantt = document.createElement('div');
+            elGantt.id = 'gantt';
+            elSpace.appendChild(elGantt);
+        }
+
+        // To switch the moon, the day of the week, depending on the login user's Locale.
+        var user = kintone.getLoginUser();
+        switch (user['language']) {
+            case "ja":
+                GANTT_MONTHS = ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'];
+                GANTT_DOW = ['日', '月', '火', '水', '木', '金', '土'];
+                GANTT_WAIT = '表示するまでお待ちください。';
+                break;
+            case "zh":
+                GANTT_MONTHS = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+                GANTT_DOW = ['日', '一', '二', '三', '四', '五', '六'];
+                GANTT_WAIT = '请等待显示屏';
+                break;
+            default:
+                GANTT_MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+                GANTT_DOW = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+                GANTT_WAIT = 'Please Wait...';
+                break;
+        }
+
+        // Set the record.
+        for (var i = 0; i < records.length; i++) {
+
+            var colorGantt = "ganttGray";
+            var arrayRed = GANTT_RED.split(",");
+            var arrayOrange = GANTT_ORANGE.split(",");
+            var arrayGreen = GANTT_GREEN.split(",");
+            var arrayBlue = GANTT_BLUE.split(",");
+            var arrayGray = GANTT_GRAY.split(",");
+
+            var colorValue = records[i][GANTT_COLOR]['value'];
+            if (colorValue === "") {
+                colorGantt = "ganttGray";
+            }
+            else if (arrayRed.indexOf(colorValue) >= 0) {
+                colorGantt = "ganttRed";
+            }
+            else if (arrayOrange.indexOf(colorValue) >= 0) {
+                colorGantt = "ganttOrange";
+            }
+            else if (arrayGreen.indexOf(colorValue) >= 0) {
+                colorGantt = "ganttGreen";
+            }
+            else if (arrayBlue.indexOf(colorValue) >= 0) {
+                colorGantt = "ganttBlue";
+            }
+            else if (arrayGray.indexOf(colorValue) >= 0) {
+                colorGantt = "ganttGray";
+            }
+            else {
+                colorGantt = "ganttGray";
+            }
+
+            var descGantt = "<b>" + escapeHtml(records[i][GANTT_NAME]['value']) + "</b>";
+            if (records[i][GANTT_FROM]['value']) {
+                descGantt += "<br/>" + "From: " + escapeHtml(records[i][GANTT_FROM]['value']);
+            }
+            if (records[i][GANTT_TO]['value']) {
+                descGantt += "<br/>" + "To: " + escapeHtml(records[i][GANTT_TO]['value']);
+            }
+            if (records[i][GANTT_COLOR]['value']) {
+                descGantt += "<br/>" + escapeHtml(records[i][GANTT_COLOR]['value']);
+            }
+
+            var obj = {
+                id: escapeHtml(records[i]['$id']['value']),
+                name: GANTT_NAME ? escapeHtml(records[i][GANTT_NAME]['value']) : '',
+                desc: GANTT_DESC ? escapeHtml(records[i][GANTT_DESC]['value']) : '',
+                values: [{
+                    from: convertDateTime(records[i][GANTT_FROM]['value']),
+                    to: convertDateTime(records[i][GANTT_TO]['value']),
+                    desc: descGantt,
+                    label: GANTT_LABEL ? escapeHtml(records[i][GANTT_LABEL]['value']) : '',
+                    customClass: escapeHtml(colorGantt)
+                }]
+            };
+
+            data.push(obj);
+        }
+
+        // Set in Gantt object.
+        $(elGantt).gantt({
+            source: data,
+            navigate: "scroll",
+            scale: GANTT_SCALL,
+            maxScale: "months",
+            minScale: "days",
+            months: GANTT_MONTHS,
+            dow: GANTT_DOW,
+            left: "70px",
+            itemsPerPage: 100,
+            waitText: GANTT_WAIT,
+            scrollToToday: true,
+
+            onItemClick: function(data) {
+                // None
+            }
+        });
+    });
+    
+    // To HTML escape
+    function escapeHtml(str) {
+        str = str.replace(/&/g, '&amp;');
+        str = str.replace(/</g, '&lt;');
+        str = str.replace(/>/g, '&gt;');
+        str = str.replace(/"/g, '&quot;');
+        str = str.replace(/'/g, '&#39;');
+        return str;
+    };
+
+    // Date conversion for Gantt.
+    function convertDateTime(str) {
+        if (str !== "") {
+            return '/Date(' + (new Date(str)).getTime() + ')/';
+        }
+        else {
+            return "";
+        }
+    }
+
+})(jQuery, kintone.$PLUGIN_ID);
