a91129dc9dcd96b116eddc02dc3396ab469986cd
Ushirosako <t-ushirosako@cybozu.co.jp>
2015-08-04 20:41:17 +0900

    Exclude the secret key mechanism and translated into English

--- package.sh
+++ package.sh
@@ -15,7 +15,7 @@ if [ -f "$2" ]; then
     PPK_FILE=$(cd $(dirname $2); pwd)/$(basename $2)
 fi
 
-# ディレクトリ、ファイルのチェック
+# Check directory and files
 if [ ! -d $PLUGIN_DIR ]; then
     echo "Plugin directory $PLUGIN_DIR not found."
     exit 1
@@ -28,14 +28,20 @@ fi
 
 DOT_FILES=`find $PLUGIN_DIR -name ".*" | grep -v "/\.$"`
 if [ "$DOT_FILES" != "" ]; then
-    echo "Dot files or directories cannot be contained."
+    echo "PLUGIN_DIR must not contain dot files or directories."
     for DOT_FILE in $DOT_FILES; do
         echo $DOT_FILE
     done
     exit 1
 fi
 
-# コマンド
+PPK_FILES=`find $PLUGIN_DIR -name "*.ppk"`
+if [ "$PPK_FILES" != "" ]; then
+    echo "PLUGIN_DIR must not contain *.ppk files."
+    exit 1
+fi
+
+# Command
 if [ -x /bin/sed ]; then
     SED="/bin/sed"
 elif [ -x /usr/bin/sed ]; then
@@ -45,7 +51,7 @@ else
     exit 1
 fi
 
-# 秘密鍵を作成
+# Create secret key
 if [ ! -f "$PPK_FILE" ]; then
     /bin/mkdir $BASE_DIR/keys >/dev/null 2>&1
     PPK_FILE=$BASE_DIR/keys/tmp.ppk
@@ -53,31 +59,31 @@ if [ ! -f "$PPK_FILE" ]; then
     /usr/bin/openssl genrsa -out $PPK_FILE 1024 >/dev/null 2>&1
 fi
 
-# パッケージ用の一時ディレクトリを作成
+# Create a temporary directory for the package
 PACKAGE_DIR=$BASE_DIR/tmp
 /bin/rm -rf $PACKAGE_DIR
 /bin/mkdir $PACKAGE_DIR
 
-# プラグインディレクトリを圧縮してコンテンツファイルを作成
+# And creating a content file by compressing the plug Directory
 CONTENTS_FILE=$PACKAGE_DIR/contents.zip
 cd $PLUGIN_DIR
 /usr/bin/zip -r $CONTENTS_FILE ./ >/dev/null
 
-# 公開鍵と署名を作成
+# Create a signature and public key
 PUB_FILE=$PACKAGE_DIR/PUBKEY
 SIG_FILE=$PACKAGE_DIR/SIGNATURE
-/usr/bin/openssl sha1 -sha1 -binary -sign $PPK_FILE < $CONTENTS_FILE > $SIG_FILE 
+/usr/bin/openssl sha1 -sha1 -binary -sign $PPK_FILE < $CONTENTS_FILE > $SIG_FILE
 /usr/bin/openssl rsa -pubout -outform DER < $PPK_FILE > $PUB_FILE 2>/dev/null
 
 UUID=`/usr/bin/openssl dgst -sha256 < $PUB_FILE | $SED 's/^.* //' | /usr/bin/cut -c 1-32 | /usr/bin/tr '0-9a-f' 'a-p'`
 
-# 秘密鍵をリネーム
+#  Rename the secret key
 if [ "$PPK_FILE_TMP" != "" ]; then
     PPK_FILE=$(cd $(dirname $PPK_FILE); pwd)/`basename $PLUGIN_DIR`.$UUID.ppk
     /bin/mv $PPK_FILE_TMP $PPK_FILE
 fi
 
-# パッケージファイルを作成
+# Create a package file
 OUTPUT_DIR=$BASE_DIR/plugins/$UUID
 /bin/mkdir $BASE_DIR/plugins >/dev/null 2>&1
 /bin/mkdir $OUTPUT_DIR > /dev/null 2>&1
@@ -88,7 +94,7 @@ OUTPUT_FILE=$OUTPUT_DIR/plugin.zip
 cd $PACKAGE_DIR
 /usr/bin/zip -r $OUTPUT_FILE ./ >/dev/null
 
-# 後片付け
+# Cleanup
 cd $BASE_DIR
 /bin/rm -rf $PACKAGE_DIR
 
