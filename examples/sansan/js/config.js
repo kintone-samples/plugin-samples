d0e4d4b05e81a486e28c05ccb827ffb4e6000d6d
masaya-chikamoto <masaya-chikamoto@cybozu.co.jp>
2016-07-14 11:08:21 +0900

    update sansan-plugin

--- examples/sansan/js/config.js
+++ examples/sansan/js/config.js
@@ -8,7 +8,6 @@
  */
 
 jQuery.noConflict();
-
 (function($, PLUGIN_ID) {
     "use strict";
 
@@ -18,16 +17,20 @@ jQuery.noConflict();
             'ja': {
                 'sansan_apikey_title': 'Sansan API Key',
                 'sansan_apikey_desc': 'Sansan の管理画面で発行した、API Keyを入力してください。',
+                'sansan_function_title': '使用する機能',
+                'sansan_function_desc': '本プラグインで使用する機能にチェックを入れてください。',
                 'sansan_spacefield_title': '検索ボタン配置スペース',
                 'sansan_spacefield_desc': 'ここで指定したスペースフィールドに、「取得」「クリア」「期間」の枠なしボタンを配置します。',
                 'sansan_keyfield_title': '検索キーフィールド',
-                'sansan_keyfield_desc': 'ここで選んだフィールドをもとに、Sansan に検索リクエストを送信します。',
+                'sansan_keyfield_desc': 'ここで選んだフィールドをもとに、Sansan に検索リクエストを送信します。<br>'+
+                                         '※レコード一括登録機能では一覧画面に検索リクエスト用のテキストボックスが表示されます。',
                 'sansan_originalfield_title': 'コピー元のSansanフィールド',
-                'sansan_originalfield_desc': 'kintone側の検索キーフィールドの値に、このSansanフィールドが該当した場合、検索結果を返します。',
+                'sansan_originalfield_desc': 'kintone側の検索キーフィールドの値に、この Sansan フィールドが該当した場合、検索結果を返します。',
                 'sansan_copy_title': 'コピー先フィールド',
                 'sansan_copy_desc': '取得先のフィールドを選択してください。',
                 'sansan_copy_owner_desc': '名刺所有者：',
                 'sansan_copy_companyname_desc': '会社名：',
+                'sansan_copy_userid_desc': '人物ID：',
                 'sansan_copy_username_desc': '氏名：',
                 'sansan_copy_departmentname_desc': '部署名：',
                 'sansan_copy_title_desc': '役職：',
@@ -35,6 +38,7 @@ jQuery.noConflict();
                 'sansan_copy_email_desc': 'E-mail：',
                 'sansan_copy_tel_desc': 'Tel：',
                 'sansan_copy_mobile_desc': '携帯：',
+                'sansan_copy_url_desc': 'URL：',
                 'error': 'エラー: ',
                 'sansan_plugin_submit_title': '     保存   ',
                 'sansan_plugin_cancel_title': '  キャンセル   ',
@@ -44,6 +48,8 @@ jQuery.noConflict();
             'en': {
                 'sansan_apikey_title': 'Sansan API Key',
                 'sansan_apikey_desc': '',
+                'sansan_function_title': 'Use function',
+                'sansan_function_desc': '',
                 'sansan_spacefield_title': 'Search button display field',
                 'sansan_spacefield_desc': 'The "Lookup" "Clear" and "Time range" buttons' +
                                             ' will be placed in the "Blank space" field' +
@@ -58,6 +64,7 @@ jQuery.noConflict();
                 'sansan_copy_title': 'Field Mappings',
                 'sansan_copy_owner_desc': 'Owner：',
                 'sansan_copy_companyname_desc': 'CompanyName：',
+                'sansan_copy_userid_desc': 'PersonID：',
                 'sansan_copy_username_desc': 'Name：',
                 'sansan_copy_departmentname_desc': 'DepartmentName：',
                 'sansan_copy_title_desc': 'Title：',
@@ -65,6 +72,7 @@ jQuery.noConflict();
                 'sansan_copy_email_desc': 'E-mail：',
                 'sansan_copy_tel_desc': 'Tel：',
                 'sansan_copy_mobile_desc': 'Mobile：',
+                'sansan_copy_url_desc': 'URL：',
                 'error': 'Error: ',
                 'sansan_plugin_submit_title': '     Save   ',
                 'sansan_plugin_cancel_title': '  Cancel   ',
@@ -93,24 +101,29 @@ jQuery.noConflict();
             //既に値が設定されている場合はフィールドに値を設定する
             if (Object.keys(conf).length !== 0) {
                 $('#sansan_api_code').val(conf['sansanapikey']);
+                $('#sansan-checkbox input[value="' + conf['sansan_lookup_flg'] + '"]').prop("checked", true);
+                $('#sansan-checkbox input[value="' + conf['sansan_postrecords_flg'] + '"]').prop("checked", true);
                 $("#sansan_spacefield_code").val(conf['spacefield']);
                 $("#sansan_keyfield_code").val(conf['keyfield']);
                 $("#sansan_originalfield_code").val(conf['originalfield']);
                 $("#sansan_copy_owner_code").val(conf['copy_owner']);
                 $("#sansan_copy_companyname_code").val(conf['copy_companyname']);
+                $("#sansan_copy_userid_code").val(conf['copy_userid']);
                 $("#sansan_copy_username_code").val(conf['copy_username']);
                 $("#sansan_copy_departmentname_code").val(conf['copy_departmentname']);
                 $("#sansan_copy_title_code").val(conf['copy_title']);
                 $("#sansan_copy_address_code").val(conf['copy_address']);
                 $("#sansan_copy_email_code").val(conf['copy_email']);
                 $("#sansan_copy_tel_code").val(conf['copy_tel']);
+                $("#sansan_copy_url_code").val(conf['copy_url']);
                 $("#sansan_copy_mobile_code").val(conf['copy_mobile']);
             }
         }
 
         function setDropdown() {
             // キーフィールド選択肢作成
-            kintone.api(kintone.api.url('/k/v1/preview/form', true), 'GET', {'app': kintone.app.getId()}, function(resp) {
+            kintone.api(kintone.api.url('/k/v1/preview/form', true), 'GET', {'app': kintone.app.getId()},
+            function(resp) {
                 for (var i = 0; i < resp.properties.length; i++) {
                     var prop = resp.properties[i];
                     var $appendhtml;
@@ -127,10 +140,14 @@ jQuery.noConflict();
                                 escapeHtml(prop.code) + '\">' + escapeHtml(prop.label) + '</option>');
                                 $('.sansan-copy-mail').append($appendhtml);
 
-                            }else if (prop.protocol === "CALL") {
+                            } else if (prop.protocol === "CALL") {
                                 $appendhtml = $('<option value = ' + '\"' +
                                 escapeHtml(prop.code) + '\">' + escapeHtml(prop.label) + '</option>');
                                 $('.sansan-copy-call').append($appendhtml);
+                            } else if (prop.protocol === "WEB") {
+                                $appendhtml = $('<option value = ' + '\"' +
+                                escapeHtml(prop.code) + '\">' + escapeHtml(prop.label) + '</option>');
+                                $('.sansan-copy-web').append($appendhtml);
                             }
                             break;
 
@@ -153,19 +170,31 @@ jQuery.noConflict();
             var header = {
                 'X-Sansan-Api-Key': $('#sansan_api_code').val()
             };
+            var lookup_flg = "";
+            var postrecords_flg = "";
+            if ($('#sansan-checkbox input[id="sansan-checkbox-lookup"]:checked')[0]) {
+                lookup_flg = $('#sansan-checkbox input[id="sansan-checkbox-lookup"]:checked')[0].value;
+            }
+            if ($('#sansan-checkbox input[id="sansan-checkbox-postrecords"]:checked')[0]) {
+                postrecords_flg = $('#sansan-checkbox input[id="sansan-checkbox-postrecords"]:checked')[0].value;
+            }
             // 設定文書の値を返す
             config['sansanapikey'] = $('#sansan_api_code').val();
+            config['sansan_lookup_flg'] = lookup_flg;
+            config['sansan_postrecords_flg'] = postrecords_flg;
             config['spacefield'] = $("#sansan_spacefield_code").val();
             config['keyfield'] = $("#sansan_keyfield_code").val();
             config['originalfield'] = $("#sansan_originalfield_code").val();
             config['copy_owner'] = $('#sansan_copy_owner_code').val();
             config['copy_companyname'] = $('#sansan_copy_companyname_code').val();
+            config['copy_userid'] = $('#sansan_copy_userid_code').val();
             config['copy_username'] = $('#sansan_copy_username_code').val();
             config['copy_departmentname'] = $('#sansan_copy_departmentname_code').val();
             config['copy_title'] = $('#sansan_copy_title_code').val();
             config['copy_address'] = $('#sansan_copy_address_code').val();
             config['copy_email'] = $('#sansan_copy_email_code').val();
             config['copy_tel'] = $('#sansan_copy_tel_code').val();
+            config['copy_url'] = $('#sansan_copy_url_code').val();
             config['copy_mobile'] = $('#sansan_copy_mobile_code').val();
 
             kintone.plugin.app.setProxyConfig("https://api.sansan.com/", "GET", header, {}, function(resp) {
