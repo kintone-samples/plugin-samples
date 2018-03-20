13cad83681074409901fd816d229e90e9516616a
dinh-tran <dinh-tran@cybozu.vn>
2018-03-20 17:35:54 +0700

    Revert code

--- examples/conditionformat2/js/config.js
+++ examples/conditionformat2/js/config.js
@@ -510,7 +510,7 @@ jQuery.noConflict();
 
         $('.cf-plugin-column5, .cf-plugin-column6').bind('paste', function(event) {
             $(this).val(event.originalEvent.clipboardData.getData('Text').trim());
-            $(this).trigger(['focus', 'change']);
+            $(this).trigger('change');
         });
 
         // Change color
