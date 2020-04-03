f3ef0347d056f87ab94ea536040e3c62ef3839a6
aki <akikaori723@gmail.com>
2020-04-03 16:15:43 +0900

    add CN Localization

--- examples/eventCalendar/js/customize.js
+++ examples/eventCalendar/js/customize.js
@@ -8,6 +8,55 @@ jQuery.noConflict();
 (function($, PLUGIN_ID) {
     'use strict';
 
+    // 文言多言語化
+    function createMultilingual(num){
+        var user_lang = kintone.getLoginUser().language;
+        var multilingual = {
+            'ja':{
+                '1': 'YYYY年M月',
+                '2': 'YYYY年 M月 D日',
+                '3': 'YYYY年 M月 D日[(]ddd[)]',
+                '4':'今日',
+                '5': '月',
+                '6': '週',
+                '7': '日',
+                '8': ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+                '9': ['日曜', '月曜', '火曜', '水曜', '木曜', '金曜', '土曜'],
+                '10': ['日', '月', '火', '水', '木', '金', '土'],
+                '11': 'すべて見る',
+                '12':'未入力項目があります',
+                '13': '必須です',
+                '14': '開始日時が終了日時より未来になっています',
+                '15': '終了日時より過去にして下さい',
+                '16': '開始日時より未来にして下さい',
+                '17': '開始日時と終了日時は1分以上あけて下さ'
+            },
+            'zh':{
+                '1': 'YYYY年M月',
+                '2': 'YYYY年 M月 D日',
+                '3': 'YYYY年 M月 D日[(]ddd[)]',
+                '4':'今天',
+                '5': '月',
+                '6': '周',
+                '7': '日',
+                '8': ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+                '9': ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+                '10': ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+                '11': '显示全部',
+                '12':'有必填项未填',
+                '13': '必填',
+                '14': '开始时间比结束时间晚',
+                '15': '请选择比结束时间早的时间',
+                '16': '请选择比开始时间晚的时间',
+                '17': '开始时间和结束时间必须间隔至少1分钟'
+            }
+        };
+        if(!multilingual[user_lang]) {
+            user_lang = 'ja';
+        }
+        return multilingual[user_lang][num];
+    }
+
     // カレンダー上でイベントの時間を変更した時の更新処理
     function putRecord(event) {
         var putConfig = kintone.plugin.app.getConfig(PLUGIN_ID);
@@ -74,9 +123,19 @@ jQuery.noConflict();
                         // イベント背景色設定でミスがある場合とプロセス管理無効の場合はデフォルト青色とする
                         var eventColor = '#0000ff';
                         // イベント背景色設定処理
-                        if (typeof (records[i].ステータス) !== 'undefined') {
-                            var eventStatus = records[i].ステータス.value;
+                        var eventStatus
+                        var flag = false;
+                        if(typeof (records[i].ステータス) !== 'undefined') {
+                            eventStatus = records[i].ステータス.value;
+                        } else if(typeof (records[i].状态) !== 'undefined'){
+                            eventStatus = records[i].状态.value;
+                        } else if(typeof (records[i].Status) !== 'undefined') {
+                            eventStatus = records[i].Status.value;
+                        } else {
+                            flag = true;
+                        }
 
+                        if(flag === false) {
                             for (var k = 1; k < 6; k++) {
                                 var stsPropName = 'status' + k;
                                 var clrPropName = 'color' + k;
@@ -87,6 +146,7 @@ jQuery.noConflict();
                                 }
                             }
                         }
+
                         recEvents.push({
                             title: records[i][evTitle].value,
                             start: startDate.format('YYYY-MM-DD HH:mm:ss'),
@@ -99,7 +159,6 @@ jQuery.noConflict();
                         });
                     }
                 }
-
 
                 // カレンダーの設定
                 $('#calendar').fullCalendar({
@@ -119,18 +178,18 @@ jQuery.noConflict();
                     },
                     // 各カレンダーのタイトル
                     titleFormat: {
-                        month: 'YYYY年M月',
-                        week: 'YYYY年 M月 D日',
-                        day: 'YYYY年 M月 D日[(]ddd[)]'
+                        month: createMultilingual('1'),
+                        week: createMultilingual('2'),
+                        day: createMultilingual('3')
                     },
                     // ボタン文字列の表記
                     buttonText: {
                         prev: '＜',
                         next: '＞',
-                        today: '今日',
-                        month: '月',
-                        week: '週',
-                        day: '日'
+                        today: createMultilingual('4'),
+                        month: createMultilingual('5'),
+                        week: createMultilingual('6'),
+                        day: createMultilingual('7')
                     },
                     // 日曜開始のカレンダーとする
                     firstDay: '0',
@@ -139,11 +198,11 @@ jQuery.noConflict();
                     // デフォルトは月カレンダー
                     defaultView: 'month',
                     // 月の表記
-                    monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-                    monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+                    monthNames: createMultilingual('8'),
+                    monthNamesShort: createMultilingual('8'),
                     // 曜日の表記
-                    dayNames: ['日曜', '月曜', '火曜', '水曜', '木曜', '金曜', '土曜'],
-                    dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+                    dayNames: createMultilingual('9'),
+                    dayNamesShort: createMultilingual('10'),
                     // 各カレンダーの各時間の表記
                     axisFormat: 'H:mm',
                     timeFormat: 'H:mm',
@@ -179,7 +238,7 @@ jQuery.noConflict();
                     scrollTime: '00:00:00',
                     // 月カレンダーでイベントが多い場合に表所を省略する
                     eventLimit: true,
-                    eventLimitText: 'すべて見る',
+                    eventLimitText: createMultilingual('11'),
                     eventResize: function(ev, delta, revertFunc, jsEvent, ui, view) {
                         putRecord(ev);
                         $('#calendar').fullCalendar('unselect');
@@ -199,6 +258,7 @@ jQuery.noConflict();
         });
     });
 
+
     // レコード作成・編集画面・一覧編集イベント
     kintone.events.on(['app.record.create.submit', 'app.record.edit.submit',
         'app.record.index.edit.submit'], function(event) {
@@ -215,26 +275,26 @@ jQuery.noConflict();
 
         // イベントタイトル・イベント開始/終了日時は未入力不可
         if (!evTitleVal || !evStartVal || !evEndVal) {
-            event.error = '未入力項目があります';
+            event.error = createMultilingual('12');
             if (!evTitleVal) {
-                record[config.name].error = '必須です';
+                record[config.name].error = createMultilingual('13');
             }
             if (!evStartVal) {
-                record[config.start_datetime].error = '必須です';
+                record[config.start_datetime].error = createMultilingual('13');
             }
             if (!evEndVal) {
-                record[config.end_datetime].error = '必須です';
+                record[config.end_datetime].error = createMultilingual('13');
             }
         // 開始日時が終了日時より未来の場合はエラー
         } else if (moment(evStartVal).format('X') > moment(evEndVal).format('X')) {
-            event.error = '開始日時が終了日時より未来になっています';
-            record[config.start_datetime].error = '終了日時より過去にして下さい';
-            record[config.end_datetime].error = '開始日時より未来にして下さい';
+            event.error = createMultilingual('14');
+            record[config.start_datetime].error = createMultilingual('15');
+            record[config.end_datetime].error = createMultilingual('16');
         // 開始日時と終了日時が同時刻の場合はエラー（FullCalendarの仕様上の問題）
         } else if (moment(evStartVal).format('X') === moment(evEndVal).format('X')) {
-            event.error = '開始日時と終了日時は1分以上あけて下さい';
-            record[config.start_datetime].error = '終了日時より過去にして下さい';
-            record[config.end_datetime].error = '開始日時より未来にして下さい';
+            event.error = createMultilingual('17');
+            record[config.start_datetime].error = createMultilingual('15');
+            record[config.end_datetime].error = createMultilingual('16');
         }
 
         return event;
